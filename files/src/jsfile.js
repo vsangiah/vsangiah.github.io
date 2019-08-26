128da3c7c3991013df3e169d636c042cb9f30b10
Borja <bpetitzarzalejos@gmail.com>
2019-08-26 14:25:45 +0200

    responsive webpage for mobile phones

--- files/src/jsfile.js
+++ files/src/jsfile.js
@@ -58,8 +58,7 @@ $(document).on("click", "a.showless", function(event){
 var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
   if (mobile) {
     $('.plus').css('display', 'none');
-    $('.hidden').css('display', 'block');
-    $('img').css('display','none') ; }
+    $('.hidden').css('display', 'block'); }
   else{  };
 
 /******************************************************************************/
