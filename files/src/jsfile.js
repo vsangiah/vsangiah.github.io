df11df50cdf1a5b7d30351933ed37165223039fa
Borja <bpetitzarzalejos@gmail.com>
2019-08-29 14:02:53 +0200

    correct few things

--- files/src/jsfile.js
+++ files/src/jsfile.js
@@ -1,31 +1,10 @@
 
-/* Code ***********************************************************************/
+/* Defined links options *******************************************************/
 
 $('a'                  ).attr('target','_blank');
 $('a[class=simple]'    ).attr('target','_self');
 $('a[class=toc]'       ).attr('target','_self');
 $('a[class=toca]'      ).attr('target','_self');
-
-/* Institutions ***************************************************************/
-
-$('a[id=cemfi]'        ).attr('href','https://www.cemfi.es/');
-$('a[id=nhh]'          ).attr('href','https://www.nhh.no/en/departments/economics/');
-
-/* Profiles *******************************************************************/
-
-$('a[id=ssrn]'         ).attr('href','https://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=3308699');
-$('a[id=google]'       ).attr('href','https://scholar.google.es/citations?user=ySzuvdUAAAAJ&hl=en');
-$('a[id=ideas]'        ).attr('href','https://ideas.repec.org/f/ppe880.html');
-$('a[id=linkedin]'     ).attr('href','https://www.linkedin.com/in/borjapetit/');
-
-/* People *********************************************************************/
-
-$('a[id=jim]'          ).attr('href','https://sites.google.com/site/jimcostain/');
-$('a[id=anton]'        ).attr('href','https://sites.google.com/site/antonnakov/');
-$('a[id=josep]'        ).attr('href','https://www.cemfi.es/~pijoan/Welcome.html');
-$('a[id=nezih]'        ).attr('href','https://www.cemfi.es/~guner/');
-$('a[id=jc]'           ).attr('href','https://www.cemfi.es/studies/phd/ruiz.asp');
-$('a[id=javi]'         ).attr('href','https://www.cemfi.es/studies/phd/lopez.asp');
 
 /* Show/Hide functions *********************************************************/
 
@@ -57,8 +36,7 @@ $(document).on("click", "a.showless", function(event){
 
 var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
   if (mobile) {
-    $('.plus').css('display', 'none');
-    $('.hidden').css('display', 'block'); }
+    $('.plus').css('display', 'none'); }
   else{  };
 
 /******************************************************************************/
---
+++ files/src/links.js
@@ -0,0 +1,48 @@
+
+/* Institutions ***************************************************************/
+
+$('a[id=cemfi]'        ).attr('href','https://www.cemfi.es/');
+$('a[id=airef]'        ).attr('href','https://www.airef.es/en/');
+
+/* Personal Info **************************************************************/
+
+$('a[id=ssrn]'         ).attr('href','https://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=3308699');
+$('a[id=google]'       ).attr('href','https://scholar.google.es/citations?user=ySzuvdUAAAAJ&hl=en');
+$('a[id=ideas]'        ).attr('href','https://ideas.repec.org/f/ppe880.html');
+$('a[id=linkedin]'     ).attr('href','https://www.linkedin.com/in/borjapetit/');
+$('a[id=twitter]'      ).attr('href','https://https://twitter.com/borjapetit');
+$('a[id=skype]'        ).attr('href','skype:borjapetit');
+
+$('a[id=email_pers]'   ).attr('href','mailto:bpetitzarzalejos@gmail.com');
+$('a[id=email_prof]'   ).attr('href','mailto:bpetit@cemfi.edu.es');
+
+/* People *********************************************************************/
+
+$('a[id=jim]'          ).attr('href','https://sites.google.com/site/jimcostain/');
+$('a[id=anton]'        ).attr('href','https://sites.google.com/site/antonnakov/');
+$('a[id=josep]'        ).attr('href','https://www.cemfi.es/~pijoan/Welcome.html');
+$('a[id=nezih]'        ).attr('href','https://www.cemfi.es/~guner/');
+$('a[id=jc]'           ).attr('href','https://www.cemfi.es/studies/phd/ruiz.asp');
+$('a[id=javi]'         ).attr('href','https://www.cemfi.es/studies/phd/lopez.asp');
+$('a[id=enriquemoral]' ).attr('href','https://moralbenito.weebly.com/');
+$('a[id=tonibraun]'    ).attr('href','https://www.frbatlanta.org/research/economists/braun-anton.aspx');
+
+/* Papers *********************************************************************/
+
+$('a[id=fertility_wp]' ).attr('href','files/papers/petit_childtax.pdf');
+
+$('a[id=firing_wp]'    ).attr('href','files/papers/petit_firing.pdf');
+
+$('a[id=lpw_wp]'       ).attr('href','files/papers/costainnakovpetit_lpw.pdf');
+$('a[id=lpw_cepr]'     ).attr('href','https://cepr.org/active/publications/discussion_papers/dp.php?dpno=13398');
+$('a[id=lpw_ecb]'      ).attr('href','https://www.ecb.europa.eu/pub/pdf/scpwps/ecb.wp2272~38362298bc.en.pdf?19821a6f04d4080adbe82c2b99c72e16');
+$('a[id=lpw_bde]'      ).attr('href','https://www.bde.es/f/webbde/SES/Secciones/Publicaciones/PublicacionesSeriadas/DocumentosTrabajo/19/Fich/dt1910e.pdf');
+
+/* Others *********************************************************************/
+
+$('a[id=macro1]'       ).attr('href','https://www.cemfi.es/studies/master/course.asp?c=101114');
+$('a[id=macro2]'       ).attr('href','https://www.cemfi.es/studies/master/course.asp?c=101118');
+$('a[id=diplomasuper]' ).attr('href','https://www.cemfi.es/studies/dsb/index.asp');
+$('a[id=mastercemfi]'  ).attr('href','https://www.cemfi.es/studies/master/index.asp');
+
+/******************************************************************************/
