699764ae05592105c71f70cd2c136a8dc22754e7
Borja <bpetitzarzalejos@gmail.com>
2019-11-24 11:54:35 +0100

    ok

--- files/src/jsfile.js
+++ files/src/jsfile.js
@@ -20,26 +20,4 @@ $(document).on("click", "a.minus", function(event){
     $(this).attr('class', 'plus');
 });
 
-$(document).on("click", "a.showmore", function(event){
-    event.preventDefault();
-    $(this).parent('p').attr('class','abstract2');
-    $(this).attr('class','showless');
-});
-
-$(document).on("click", "a.showless", function(event){
-    event.preventDefault();
-    $(this).parent('p').attr('class','abstract');
-    $(this).attr('class','showmore');
-});
-
-/* Make hide/show elements visible when using a mobile device *****************/
-
-/*
-var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-  if (mobile) {
-    $('.plus').css('display', 'none'); }
-    $('.hidden').css('display', 'block'); }
-  else{  };
-*/
-
 /******************************************************************************/
