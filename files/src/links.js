0434908d47187054277bc201992572b3a4303409
Borja <bpetitzarzalejos@gmail.com>
2020-03-05 21:52:11 +0100

    new page for the vitae

--- files/src/links.js
+++ files/src/links.js
@@ -2,19 +2,9 @@
 /* Institutions ***************************************************************/
 
 $('a[id=cemfi]'        ).attr('href','https://www.cemfi.es/');
+$('a[id=cunef]'        ).attr('href','https://www.cunef.edu/en/');
 $('a[id=airef]'        ).attr('href','https://www.airef.es/en/');
-
-/* Personal Info **************************************************************/
-
-$('a[id=ssrn]'         ).attr('href','https://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=3308699');
-$('a[id=google]'       ).attr('href','https://scholar.google.es/citations?user=ySzuvdUAAAAJ&hl=en');
-$('a[id=ideas]'        ).attr('href','https://ideas.repec.org/f/ppe880.html');
-$('a[id=linkedin]'     ).attr('href','https://www.linkedin.com/in/borjapetit/');
-$('a[id=twitter]'      ).attr('href','https://twitter.com/borjapetit');
-$('a[id=skype]'        ).attr('href','skype:borjapetit');
-
-$('a[id=email_pers]'   ).attr('href','mailto:bpetitzarzalejos@gmail.com');
-$('a[id=email_prof]'   ).attr('href','mailto:bpetit@cemfi.edu.es');
+$('a[id=ucm]'          ).attr('href','https://economicasyempresariales.ucm.es');
 
 /* People *********************************************************************/
 
@@ -30,10 +20,7 @@ $('a[id=tonibraun]'    ).attr('href','https://www.frbatlanta.org/research/econom
 /* Papers *********************************************************************/
 
 $('a[id=fertility_wp]' ).attr('href','files/papers/petit_childtax.pdf');
-
 $('a[id=firing_wp]'    ).attr('href','files/papers/petit_firing.pdf');
-$('a[id=firing_code]'  ).attr('href','files/papers/petit_firing_code.zip');
-
 $('a[id=lpw_wp]'       ).attr('href','files/papers/costainnakovpetit_lpw.pdf');
 $('a[id=lpw_cepr]'     ).attr('href','https://cepr.org/active/publications/discussion_papers/dp.php?dpno=13398');
 $('a[id=lpw_ecb]'      ).attr('href','https://www.ecb.europa.eu/pub/pdf/scpwps/ecb.wp2272~38362298bc.en.pdf?19821a6f04d4080adbe82c2b99c72e16');
@@ -45,5 +32,3 @@ $('a[id=macro1]'       ).attr('href','https://www.cemfi.es/studies/master/course
 $('a[id=macro2]'       ).attr('href','https://www.cemfi.es/studies/master/course.asp?c=101118');
 $('a[id=diplomasuper]' ).attr('href','https://www.cemfi.es/studies/dsb/index.asp');
 $('a[id=mastercemfi]'  ).attr('href','https://www.cemfi.es/studies/master/index.asp');
-
-/******************************************************************************/
