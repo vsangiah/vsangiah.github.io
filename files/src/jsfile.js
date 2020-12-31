371c75618ac8c5dcfd80179b83c595677843231e
Borja <bpetitzarzalejos@gmail.com>
2020-12-31 15:52:10 +0100

    ok

--- files/src/jsfile.js
+++ files/src/jsfile.js
@@ -17,23 +17,6 @@ $('a[id=javi]'         ).attr('href','https://www.cemfi.es/studies/phd/lopez.asp
 $('a[id=enriquemoral]' ).attr('href','https://moralbenito.weebly.com/');
 $('a[id=tonibraun]'    ).attr('href','https://www.frbatlanta.org/research/economists/braun-anton.aspx');
 
-/* Papers *********************************************************************/
-
-$('a[id=fertility_wp]' ).attr('href','files/papers/petit_childtax.pdf');
-$('a[id=firing_wp]'    ).attr('href','files/papers/petit_firing.pdf');
-$('a[id=firing_code]'  ).attr('href','files/papers/petit_firing_codes.zip');
-$('a[id=lpw_wp]'       ).attr('href','files/papers/costainnakovpetit_lpw.pdf');
-$('a[id=lpw_cepr]'     ).attr('href','https://cepr.org/active/publications/discussion_papers/dp.php?dpno=13398');
-$('a[id=lpw_ecb]'      ).attr('href','https://www.ecb.europa.eu/pub/pdf/scpwps/ecb.wp2272~38362298bc.en.pdf?19821a6f04d4080adbe82c2b99c72e16');
-$('a[id=lpw_bde]'      ).attr('href','https://www.bde.es/f/webbde/SES/Secciones/Publicaciones/PublicacionesSeriadas/DocumentosTrabajo/19/Fich/dt1910e.pdf');
-
-/* Others *********************************************************************/
-
-$('a[id=macro1]'       ).attr('href','https://www.cemfi.es/studies/master/course.asp?c=101114');
-$('a[id=macro2]'       ).attr('href','https://www.cemfi.es/studies/master/course.asp?c=101118');
-$('a[id=diplomasuper]' ).attr('href','https://www.cemfi.es/studies/dsb/index.asp');
-$('a[id=mastercemfi]'  ).attr('href','https://www.cemfi.es/studies/master/index.asp');
-
 /* Defined links options *******************************************************/
 
 $('a'                  ).attr('target','_blank');
